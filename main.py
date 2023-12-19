c4da811151e4ac639ca5c7aa48e430b3361ec7b0
Aria Doustkhah <aria.vajarii@gmail.com>
2023-12-19 17:48:31 +0330

    sentence to paragraph

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from math import log2, sqrt
 import string
-import re
 
 
 class Document():
     def __init__(self):
         self.text = ""
-        self.sentences_vectors = {}
+        self.paragraphs_vectors = {}
         self.doc_vector = []
 
 
@@ -47,18 +46,20 @@ def make_inverted_index(documents_list):
     return inverted_index
 
 
-def TF_IDF_vectorize(documents_list: list, inverted_index: dict, sentence):
-    tokenized_sentence = tokenizer(sentence)
+def TF_IDF_vectorize(documents_list: list, inverted_index: dict, paragraph):
+    tokenized_paragraph = tokenizer(paragraph)
     vector = []
     for word in inverted_index.keys():
         vector.append(TF_IDF_calculator(documents_list,
-                      inverted_index, tokenized_sentence, word))
+                      inverted_index, tokenized_paragraph, word))
     return vector
 
 
 def vectorizing_documents(documents_list: dict):
     for doc in documents_list:
-        doc.doc_vector = [sum(x) for x in zip(*doc.sentences_vectors.values())]
+        doc.doc_vector = [sum(x)
+                          for x in zip(*doc.paragraphs_vectors.values())]
+
 
 def cosine_similarity_calculator(A: list, B: list):
     sumAB, sumA2, sumB2 = 0, 0, 0
@@ -67,6 +68,7 @@ def cosine_similarity_calculator(A: list, B: list):
         sumA2 += A[i]*A[i]
         sumB2 += B[i]*B[i]
     return sumAB/(sqrt(sumA2)*sqrt(sumB2))
+
 
 def most_similar(documents_list: dict, query_vector: list):
     cosine_distance = {}
@@ -78,17 +80,24 @@ def most_similar(documents_list: dict, query_vector: list):
         cosine_distance.items(), key=lambda item: item[1]))
     return cosine_distance
 
+# storing paragraphs of the document selected
+
+
+def paragraphs(most_similar_doc):
+    paragraph = most_similar_doc.text.split('\n')
+    return paragraph
+
 
 documents_list = []
 
 corpus = []
 
-#getting the query
+# getting the query
 user_query = input("Please insert your query: ")
 
-#for storing the document's id to have an access at the end
+# for storing the document's id to have an access at the end
 document_add_list = []
-#appending documents to corpus
+# appending documents to corpus
 print("candidate_document_id: ")
 for i in range(0, 20):
     document_add = int(input())
@@ -103,33 +112,46 @@ for i in range(0, 20):
 for txt in corpus:
     doc = Document()
     doc.text = txt
-    doc.sentences_vectors = {sentence: []
-                             for sentence in re.split(r'[.!?\n] ', txt)}
+    doc.paragraphs_vectors = {paragraph: []for paragraph in txt.split('\n')}
     documents_list.append(doc)
 
 inverted_index = make_inverted_index(corpus)
 
+
 for doc in documents_list:
-    for sentence in doc.sentences_vectors.keys():
-        doc.sentences_vectors[sentence] = TF_IDF_vectorize(
-            corpus, inverted_index, sentence)
+    for paragraph in doc.paragraphs_vectors.keys():
+        doc.paragraphs_vectors[paragraph] = TF_IDF_vectorize(
+            corpus, inverted_index, paragraph)
 
 vectorizing_documents(documents_list)
 
-user_query = input()
+
 user_query_vector = TF_IDF_vectorize(corpus, inverted_index, user_query)
 
 most_similar_doc = list(most_similar(
     documents_list, user_query_vector).keys())[-1]
 
-print("most_similar_doc.text: ", most_similar_doc.text)
-cosine_distance = {}
-for sentence in most_similar_doc.sentences_vectors.keys():
-    sentence_vector = most_similar_doc.sentences_vectors[sentence]
-    cosine = cosine_similarity_calculator(user_query_vector, sentence_vector)
-    cosine_distance[sentence] = cosine
+# getting access to the index that the text is stored and the documen's id next
+wnated_index = corpus.index(most_similar_doc.text)
+print("document_id: ", document_add_list[wnated_index])
 
-cosine_distance = dict(sorted(
-    cosine_distance.items(), key=lambda item: item[1]))
-most_similar_sentence = list(cosine_distance.keys())[-1]
-print("most_similar_sentence: ", most_similar_sentence)
+cosine_distance = {}
+for paragraph in most_similar_doc.paragraphs_vectors.keys():
+    paragraph_vector = most_similar_doc.paragraphs_vectors[paragraph]
+    cosine = cosine_similarity_calculator(user_query_vector, paragraph_vector)
+    cosine_distance[paragraph] = cosine
+
+cosine_distance = dict(
+    sorted(cosine_distance.items(), key=lambda item: item[1]))
+most_similar_paragraph = list(cosine_distance.keys())[-1]
+print("most_similar_paragraph: ", most_similar_paragraph)
+
+# finding the most similar paragraph that query used in among all the paragraph's of the selected document
+paragraph_list = paragraphs(most_similar_doc)
+print("is_selected: ")
+
+for i in paragraph_list:
+    if i == most_similar_paragraph:
+        print("     1")
+    else:
+        print("     0")
