3f1491b272b866a272265e9eff776d32dd1102c2
Aria Doustkhah <aria.vajarii@gmail.com>
2023-12-19 16:44:55 +0330

    defining TF_IDF calculation functions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import log2
 import string
 
 
@@ -11,3 +12,15 @@ def tokenizer(text: str):
     text = text.lower()
     cleaned_text = remove_punctuation(text)
     return cleaned_text.split()
+
+
+def TF_calculator(text_words: list, word: str):
+    return text_words.count(word)
+
+
+def IDF_calculator(document_list: list, inverted_index: dict, word: str):
+    return log2(len(document_list)/len(inverted_index[word]))
+
+
+def TF_IDF_calculator(document_list: list, inverted_index: dict, document_words_list: list, word: str):
+    return float(TF_calculator(document_words_list, word))*float(IDF_calculator(document_list, inverted_index, word))
