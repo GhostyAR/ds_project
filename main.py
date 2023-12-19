6759e1c8b8950bb3d0f45f6006d51b1aaa14612c
Aria Doustkhah <aria.vajarii@gmail.com>
2023-12-19 16:49:12 +0330

    adding test data and making class for docs

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from math import log2
 import string
+import re
+
+
+class Document():
+    def __init__(self):
+        self.text = ""
+        self.sentences_vectors = {}
+        self.doc_vector = []
 
 
 def remove_punctuation(text: str):
@@ -24,3 +32,20 @@ def IDF_calculator(document_list: list, inverted_index: dict, word: str):
 
 def TF_IDF_calculator(document_list: list, inverted_index: dict, document_words_list: list, word: str):
     return float(TF_calculator(document_words_list, word))*float(IDF_calculator(document_list, inverted_index, word))
+
+
+documents_list = []
+
+corpus = [
+    "this is the first document. second document coming next.",
+    "This document is the second document. going for the third!",
+    "And this is the third one.",
+    "Is this the first document?",
+]
+
+for txt in corpus:
+    doc = Document()
+    doc.text = txt
+    doc.sentences_vectors = {sentence: []
+                             for sentence in re.split(r'[.!?\n] ', txt)}
+    documents_list.append(doc)
